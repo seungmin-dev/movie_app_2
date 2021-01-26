9cf09c4284cdb7f0ccef976ebccce1e0d8a63d64
seungmin_dev <seungmin_dev@gmail.com>
2021-01-27 06:39:35 +0900

    오류난 부분 찾아보기

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import React from "react";
 //import './App.css';
 //import Potato from "./Potato.js"; //"./"은 같은 폴더 안에 들어있다는 말
 
-function Food({fav}) {
-  return <h3>I like {fav}</h3>
+function FoodILike({name, color}) {
+  return <h3>I like {name} and it's {color}</h3>
+}
+
+const food = [
+  {name : "kimbap", color : "black"},
+  {name : "kimchi", color : "red"},
+  {name : "tteocbokki", color : "red"},
+  {name : "bibimbap", color : "colorful"}
+];
+
+function renderFood(food) {
+    return <FoodILike name={food.name} color={food.color} />;
 }
 
 function App() {
-  return <Food fav="puppies" />
+  return { FoodILike.map(renderFood) };
 }
 
 export default App;
