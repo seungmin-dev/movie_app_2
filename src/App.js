9297b6710a0a7bffe3697c4edb2af17407087b7b
seungmin_dev <seungmin_dev@gmail.com>
2021-01-27 06:25:54 +0900

    Reinitializing movie_app_2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 //import logo from './logo.svg';
 //import './App.css';
+//import Potato from "./Potato.js"; //"./"은 같은 폴더 안에 들어있다는 말
+
+function Food({fav}) {
+  return <h3>I like {fav}</h3>
+}
 
 function App() {
-  return <div className="App"><h1>Hello~~!!!</h1></div>
+  return <Food fav="puppies" />
 }
 
 export default App;
---
+++ src/Potato.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Potato() {
+    return <h3>I hate Potatos!</h3>
+}
+
+export default Potato;
