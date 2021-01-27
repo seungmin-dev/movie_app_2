17fb6369fd1487e743b911a1a276e1161e2e1ec0
seungmin_dev <seungmin_dev@gmail.com>
2021-01-27 21:52:16 +0900

    #2 JSX & PROPS

--- src/App.js
+++ src/App.js
@@ -2,24 +2,33 @@ import React from "react";
 //import logo from './logo.svg';
 //import './App.css';
 //import Potato from "./Potato.js"; //"./"은 같은 폴더 안에 들어있다는 말
+import PropTypes from "prop-types";
 
-function FoodILike({name, color}) {
+function ILike({name, color}) {
   return <h3>I like {name} and it's {color}</h3>
 }
 
-const food = [
-  {name : "kimbap", color : "black"},
-  {name : "kimchi", color : "red"},
-  {name : "tteocbokki", color : "red"},
-  {name : "bibimbap", color : "colorful"}
+const Food = [
+  {id: 1, name : "kimbap", color : "black"},
+  {id: 2, name : "kimchi", color : "red"},
+  {id: 3, name : "tteocbokki", color : "red"},
+  {id: 4, name : "bibimbap", color : "colorful"}
 ];
 
-function renderFood(food) {
-    return <FoodILike name={food.name} color={food.color} />;
-}
+ILike.propTypes = {
+  name: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired
+};
+
+// function renderFood(food) {
+//     return <ILike name={food.name} color={food.color} />;
+// }
 
 function App() {
-  return { FoodILike.map(renderFood) };
+  return <div>{ Food.map(dish => (
+    <ILike key={dish.id} name={dish.name} color={dish.color} />
+  )) 
+  }</div>
 }
 
 export default App;
