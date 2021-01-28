02786fed723d3e19de6f8a0d5fa83758b0441d7e
seungmin_dev <seungmin_dev@gmail.com>
2021-01-28 22:51:47 +0900

    Getting API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from "react";
+import axios from "axios";
+import Movie from './Movies';
 
 class App extends React.Component { 
   state = {
-    isLoading : true
+    isLoading : true,
+    movies : []
   };
+  getMovies = async () => { //비동기함수
+    const { data : { data : { movies } } } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating"); //axios가 끝날때까지 기다리기
+    this.setState({ movies, isLoading : false });
+  }
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading : false }); 
-    }, 6000);
+    this.getMovies();
   };
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading" : "We are ready"}</div>;  
+    const { isLoading, movies } = this.state; //isLoading 옆에 movies를 선언?해주지 않으면 밑에서 this.state.movies 이렇게 장황하게 써야함
+    return (
+      <div>
+        {isLoading ? "Loading..." : movies.map(movie => (
+          <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} /> //map에서는 항상 무언가를 return해줘야함
+        ))}
+      </div>
+    ); 
   };
 }
 
