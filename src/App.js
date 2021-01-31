3b20f71a9f944e26580ca184ead8a3413f09eeb6
seungmin_dev <seungmin.dev@gmail.com>
2021-01-31 22:33:28 +0900

    Closing the loop roughly. TODO - Making up for it later

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state; //isLoading 옆에 movies를 선언?해주지 않으면 밑에서 this.state.movies 이렇게 장황하게 써야함
     return (
-      <section class="containter">
-        {isLoading ? (<div class="loader"><span class="loader__text">loading...</span></div>) : movies.map(movie => (
-          <div class="movies">
-            <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
+      <section className="containter">
+        {isLoading ? (<div className="loader"><span className="loader__text">loading...</span></div>) : movies.map(movie => (
+          <div className="movies">
+            <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres} />
           </div>
         ))}
       </section>
--- src/Movies.js
+++ src/Movies.js
@@ -2,23 +2,34 @@ import React from "react";
 import propTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({id, year, title, summary, poster}) {
-    return <div class="movie">
+function Movie({id, year, title, summary, poster, genres}) {
+    return <div className="movie"> 
         <img src={poster} alt={title} title={title} />
-        <div class="movie__data">
-            <h3 class="movie__title">{title}</h3>
-            <h5 class="movie__year">{year}</h5>
-            <p class="movie_summary">{summary}</p>
+        <div className="movie__data">
+            <h3 className="movie__title">{title}</h3>
+            <h5 className="movie__year">{year}</h5>
+            <ul className="movie__genres">
+                {genres.map((genre, index) => { //map함수에선 index를 제공함
+                    return (
+                        <li key={index} className="genres__genre">{genre}</li>
+                    )
+                })}
+            </ul>
+            <p className="movie_summary">{summary}</p>
         </div>
     </div>;
 }
+
+// JSX에서 class를 그냥 class라고 하면 react의 class와 혼돈이 돼 오류발생 -> className
+// 비슷하게 <label for= ~~>은 HTML, for는 JS에서 반복문(loop)을 의미, <label htmlFor= ~~>라고 해야함
 
 Movie.propTypes = {
     id : propTypes.number.isRequired,
     year : propTypes.number.isRequired,
     title : propTypes.string.isRequired,
     summary : propTypes.string.isRequired,
-    poster : propTypes.string.isRequired
+    poster : propTypes.string.isRequired,
+    genres : propTypes.arrayOf(propTypes.string).isRequired
 };
 
 export default Movie;
